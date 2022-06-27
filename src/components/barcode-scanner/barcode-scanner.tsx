68a0bcf0bf81172085d000ef93ca3f20f8f99372
xulihang <xulihanghai@163.com>
2022-06-27 13:21:58 +0800

    add dynamsoft barcode reader to decode frames

--- src/components/barcode-scanner/barcode-scanner.tsx
+++ src/components/barcode-scanner/barcode-scanner.tsx
@@ -1,4 +1,5 @@
-import { Component, h, Method, State } from '@stencil/core';
+import { Component, h } from '@stencil/core';
+import { BarcodeReader } from 'dynamsoft-javascript-barcode';
 
 @Component({
   tag: 'barcode-scanner',
@@ -11,11 +12,53 @@ export class BarcodeScanner {
   localStream!: MediaStream;
   cameraSelect!: HTMLSelectElement;
   camera!: HTMLVideoElement;
+  reader:BarcodeReader;
+  interval:any;
+  decoding:boolean = false;
+
+  async connectedCallback() {
+    console.log("connected");
+    BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.0.2/dist/";
+    this.reader = await BarcodeReader.createInstance();
+  }
+
+  componentDidLoad(){
+    this.loadDevicesAndPlay();
+  }
+
+  disconnectedCallback() {
+    console.log("dis connected");
+  }
 
   onCameraChanged(){
     var deviceId = this.cameraSelect.selectedOptions[0].value;
     this.play(deviceId);
   }
+
+  onCameraOpened() {
+    console.log("on opened");
+    this.startDecoding();
+  }
+
+  startDecoding(){
+    clearInterval(this.interval);
+    this.decoding = false;
+    const decode = async () => {
+      if (this.decoding === false) {
+        this.decoding = true;
+        var results = await this.reader.decode(this.camera);
+        this.decoding = false;
+        console.log(results);
+      }
+    }
+    this.interval = setInterval(decode, 500);
+  }
+
+  stopDecoding() {
+    clearInterval(this.interval);
+    this.decoding = false;
+  }
+
 
   loadDevicesAndPlay(){
     console.log(this.cameraSelect);
@@ -86,6 +129,7 @@ export class BarcodeScanner {
       if (this.localStream){
         this.localStream.getTracks().forEach(track => track.stop());
       }
+      this.stopDecoding();
     } catch (e){
       alert(e.message);
     }
@@ -96,26 +140,13 @@ export class BarcodeScanner {
     this.scanner.style.display = "none";
   };
 
-  connectedCallback() {
-    console.log("connected");
-  }
-
-  componentDidLoad(){
-
-    this.loadDevicesAndPlay();
-  }
-
-  disconnectedCallback() {
-    console.log("dis connected");
-  }
-
   render() {
     return (
       <div class="scanner" ref={(el) => this.scanner = el}>
         <select onChange={() => this.onCameraChanged()}  id="cameraSelect" ref={(el) => this.cameraSelect = el as HTMLSelectElement}></select>
         <button onClick={() => this.close()} id="closeButton">Close</button>
         <svg class="overlay fullscreen"></svg>
-        <video class="camera fullscreen" ref={(el) => this.camera = el as HTMLVideoElement} muted autoplay="autoplay" playsinline="playsinline" webkit-playsinline></video>
+        <video class="camera fullscreen" ref={(el) => this.camera = el as HTMLVideoElement} onLoadedData={()=>this.onCameraOpened()} muted autoplay="autoplay" playsinline="playsinline" webkit-playsinline></video>
       </div>
     );
   }
